6ab01d0f45b08f730832adf70f910762b9ef2b41
Ben Yang <beyang@adobe.com>
2021-12-23 00:31:16 -0800

    updated gh actions to test pypy and other platforms

--- setup.py
+++ setup.py
@@ -2,26 +2,31 @@ import sys
 from setuptools import setup, Extension
 
 with open("README.md") as f:
-       long_description = f.read()
+    long_description = f.read()
 
-main_module = Extension('unicodedataplus',
-                    sources = ['./unicodedataplus/unicodedata.c',
-                               './unicodedataplus/unicodectype.c',
-                               './unicodedataplus/pypy_ctype.c'],
-                    include_dirs = ['./unicodedataplus/', './unicodedataplus/'],
+module_sources = ["./unicodedataplus/unicodedata.c", "./unicodedataplus/unicodectype.c"]
+
+is_pypy = hasattr(sys, "pypy_version_info")
+if is_pypy:
+    module_sources.append("./unicodedataplus/pypy_ctype.c")
+main_module = Extension(
+    "unicodedataplus",
+    sources=module_sources,
+    include_dirs=["./unicodedataplus/"],
 )
 
-setup (name = "unicodedataplus",
-       version = "14.0.0",
-       description = "Unicodedata with extensions for additional properties.",
-       ext_modules = [main_module],
-       author="Ben Yang",
-       author_email="benayang@gmail.com",
-       download_url="http://github.com/iwsfutcmd/unicodedataplus",
-       license="Apache License 2.0",
-       platforms=['any'],
-       url="http://github.com/iwsfutcmd/unicodedataplus",
-       test_suite="tests",
-       long_description=long_description,
-       long_description_content_type="text/markdown",
+setup(
+    name="unicodedataplus",
+    version="14.0.0",
+    description="Unicodedata with extensions for additional properties.",
+    ext_modules=[main_module],
+    author="Ben Yang",
+    author_email="benayang@gmail.com",
+    download_url="http://github.com/iwsfutcmd/unicodedataplus",
+    license="Apache License 2.0",
+    platforms=["any"],
+    url="http://github.com/iwsfutcmd/unicodedataplus",
+    test_suite="tests",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 )
