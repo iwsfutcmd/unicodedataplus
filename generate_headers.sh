0c7147ffcfbf09c40d381f18ce4dac08e8caae84
Ben Joeng <ben.joeng@cartesia.ai>
2026-02-04 15:06:38 -0800

    revert

--- generate_headers.sh
+++ generate_headers.sh
@@ -6,6 +6,10 @@ CPYTHON_DIR="$1"
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 UNICODEDATA_C="${SCRIPT_DIR}/unicodedataplus/unicodedata.c"
 UNICODEDATA_312_C="${SCRIPT_DIR}/unicodedataplus/unicodedata.3.12.c"
+CLINIC_OUTPUT_DIR="${SCRIPT_DIR}/unicodedataplus/clinic"
+UNICODEDATA_C_H="${SCRIPT_DIR}/unicodedataplus/unicodedata.c.h"
+UNICODEDATA_312_C_H="${SCRIPT_DIR}/unicodedataplus/unicodedata.3.12.c.h"
+
 CLINIC_PY="${CPYTHON_DIR}/Tools/clinic/clinic.py"
 
 sed '/@permit_long_summary/d' "$UNICODEDATA_C" > "$UNICODEDATA_312_C" 
@@ -15,8 +19,12 @@ git checkout 41e844a
 
 python3 "$CLINIC_PY" "$UNICODEDATA_312_C"
 
+mv "$CLINIC_OUTPUT_DIR/unicodedata.3.12.c.h" "$UNICODEDATA_312_C_H"
+
 rm "$UNICODEDATA_312_C"
 
 git checkout main
 
 python3 "$CLINIC_PY" "$UNICODEDATA_C"
+
+mv "$CLINIC_OUTPUT_DIR/unicodedata.c.h" "$UNICODEDATA_C_H"
